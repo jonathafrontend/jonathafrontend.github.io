d32229d602f4ca8c41672898ac013f63cd842dd5
Jonatha Leite <jonathadasilvaleite1@gmail.com>
2024-01-29 01:18:52 -0300

    removendo arquivos remotos

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(()=>{
-    $('.btn-modern').on('click', (e)=>{
-        console.log(e.target.id)
-    })
-
-    $('#fecharAviso').on('click', (e) =>{
-        $('#aviso').fadeOut('medium')
-    })
-})
